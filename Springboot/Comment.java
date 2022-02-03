49edb429a6eea8d17973dbcad255a1df4d99765e
Bmeshanko <bmeshank@purdue.edu>
2022-02-03 17:27:23 -0500

    Wrote constructor + fields in Comment.java

--- Springboot/Comment.java
+++ Springboot/Comment.java
@@ -1,4 +1,14 @@
 package Springboot;
 public class Comment {
-    
+    private String text;
+    private Profile author;
+    private Comment[] replies;
+    private Post post;
+    private Comment parent;
+    public Comment(String text, Profile author, Post post, Comment parent) {
+        this.text = text;
+        this.author = author;
+        this.post = post;
+        this.parent = parent;
+    }
 }
