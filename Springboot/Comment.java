6fa81d78c71a286cf4433651272fb2a3994fac62
Bmeshanko <bmeshank@purdue.edu>
2022-02-03 17:33:11 -0500

    Commenting code - Github being strange on VSCODE

--- Springboot/Comment.java
+++ Springboot/Comment.java
@@ -3,12 +3,30 @@ public class Comment {
     private String text;
     private Profile author;
     private Comment[] replies;
+    private int repliesCount;
     private Post post;
     private Comment parent;
+
     public Comment(String text, Profile author, Post post, Comment parent) {
         this.text = text;
         this.author = author;
         this.post = post;
         this.parent = parent;
+
+        // This will add the comment as a reply on the parent.
+        if (parent != null) {
+            parent.addReply(this);
+        }
+    }
+
+    public void addReply(Comment c) {
+        if (repliesCount == replies.length) {
+            Comment[] newReplies = new Comment[repliesCount * 2];
+            for (int i = 0; i < repliesCount; i++) {
+                newReplies[i] = replies[i];
+            }
+            replies = newReplies;
+        }
+        replies[repliesCount++] = c;
     }
 }
