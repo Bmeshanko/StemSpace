308ae701c1dc08552e2e9fc51f7c991246a97c08
Bmeshanko <bmeshank@purdue.edu>
2022-02-03 17:02:10 -0500

    Wrote instance fields.

--- Springboot/Profile.java
+++ Springboot/Profile.java
@@ -1,4 +1,26 @@
 package Springboot;
 public class Profile {
-    
+    private String id;
+    private String name;
+    private String email;
+    private String password; // Serializable?
+    private Profile[] followers;
+    private int followersCount;
+    private Profile[] following;
+    private int followingCount;
+    private Profile[] blockedUsers;
+    private int blockedUsersCount;
+    private Profile[] mutedUsers;
+    private int mutedUsersCount;
+    private Topic[] followingTopics;
+    private int followngTopicsCount;
+    private Post[] likedPosts;
+    private int likedPostsCount;
+    private Comment[] comments;
+    private int commentsCount;
+
+    public Profile() {
+        // Needs plenty of parameters.
+    }
+
 }
