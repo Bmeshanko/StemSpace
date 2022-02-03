08257b87be35f97e157e9ae22a2795dd01a5e458
Bmeshanko <bmeshank@purdue.edu>
2022-02-03 17:06:42 -0500

    Getters and Setters for username, name, email, password

--- Springboot/Profile.java
+++ Springboot/Profile.java
@@ -20,10 +20,10 @@ public class Profile {
     private int commentsCount;
 
     public Profile(String username, String name, String email, String password) {
-        this.username = username;
-        this.name = name;
-        this.email = email;
-        this.password = password;
+        this.setUsername(username);
+        this.setName(name);
+        this.setEmail(email);
+        this.setPassword(password);
         this.followers = new Profile[200];
         this.following = new Profile[200];
         this.blockedUsers = new Profile[10];
@@ -31,5 +31,37 @@ public class Profile {
         this.followingTopics = new Topic[20];
         this.likedPosts = new Post[200];
         this.comments = new Comment[100];
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
