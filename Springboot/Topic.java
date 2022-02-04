fe972669b11ece50a1c5a28b7febb6a6a97b6c5f
Bmeshanko <bmeshank@purdue.edu>
2022-02-03 23:36:24 -0500

    Instance fields + constructor for Topic.java

--- Springboot/Topic.java
+++ Springboot/Topic.java
@@ -1,4 +1,16 @@
 package Springboot;
 public class Topic {
-    
+    private String name;
+    private Post[] posts;
+    private int postCount;
+    private Profile[] followers;
+    private int followersCount;
+
+    public Topic(String name) {
+        this.name = name;
+        this.posts = new Post[1000];
+        this.followers = new Profile[1000];
+        this.postCount = 0;
+        this.followersCount = 0;
+    }
 }
