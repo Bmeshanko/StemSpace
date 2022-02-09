c11e120442ccf63c8fe9b6dc7c73adf44883e0e4
Bmeshanko <bmeshank@purdue.edu>
2022-02-09 17:16:22 -0500

    Created basic Timeline.java - Most of this is for React.

---
+++ Springboot/Timeline.java
@@ -0,0 +1,13 @@
+package Springboot;
+public class Timeline {
+    private Profile profile;
+
+    // Constructor called when refreshing/loading timeline.
+    public Timeline(Profile profile) {
+
+    }
+
+    public void calculateOrder() {
+        
+    }
+}
