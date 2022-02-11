a4e2ad5304a5b7d0d5c420b006bb9e7d067472ba
zach-martin <mart1938@purdue.edu>
2022-02-11 17:18:17 -0500

    Just messing Around and Stuff

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -10,12 +10,35 @@ const Welcome = () => {
     );
 }
 
+const Login_button = () => (
+        <>
+        <header className='toLogin'>
+
+            Login
+
+        </header>
+        </>
+    );
+
+    const Signin_button = () => (
+        <>
+        <header className='toSignin'>
+
+            Signup
+
+        </header>
+        </>
+    );
+
 function Home() {
     return (
         <div className="Home">
             <header className="Home-header">
             <img src="Logo_new.png" alt="STEM"></img>
+
             <Welcome></Welcome>
+            <Login_button> </Login_button>
+            <Signin_button></Signin_button>
                 <a
                     className="Github-Link"
                     href="https://github.com/Bmeshanko/StemSpace"
