3736f111a68b6e2e3a215b35199f65bfd90b2412
Brandon Huang <huan1533@purdue.edu>
2022-02-13 19:45:58 -0500

    Finished comment class: added timestamp, comments, and getters

--- Springboot/Comment.java
+++ Springboot/Comment.java
@@ -1,4 +1,6 @@
 package Springboot;
+import java.sql.Timestamp;
+
 public class Comment {
     private String text;
     private Profile author;
@@ -6,12 +8,14 @@ public class Comment {
     private int repliesCount;
     private Post post;
     private Comment parent;
+    private Timestamp time;
 
     public Comment(String text, Profile author, Post post, Comment parent) {
         this.text = text;
         this.author = author;
         this.post = post;
         this.parent = parent;
+        this.time = new Timestamp(System.currentTimeMillis());
 
         // This will add the comment as a reply on the parent.
         if (parent != null) {
@@ -31,7 +35,20 @@ public class Comment {
         return text;
     }
 
+    public Timestamp getTime(){
+        return time;
+    }
+
+    public Post getPost(){
+        return post;
+    }
+
+    public Comment[] getReplies(){
+        return replies;
+    }
+
     public void addReply(Comment c) {
+        //if replies array is full, double the array size
         if (repliesCount == replies.length) {
             Comment[] newReplies = new Comment[repliesCount * 2];
             for (int i = 0; i < repliesCount; i++) {
@@ -44,6 +61,7 @@ public class Comment {
     }
 
     public void removeReply(Comment c) {
+        //if replies array less than half full, shorten array size
         if (repliesCount * 2 < replies.length) {
             Comment[] newReplies = new Comment[repliesCount + 1];
             for (int i = 0; i < repliesCount; i++) {
@@ -53,6 +71,7 @@ public class Comment {
             replies = newReplies;
         }
 
+        //find comment in replies array and remove it
         for (int i = 0; i < repliesCount; i++) {
             if (replies[i].equals(c)) {
                 while (i < repliesCount - 1) {
