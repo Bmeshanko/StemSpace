32bdcf52986bad1117c4d0740cb8c05b421f39f6
Bmeshanko <bmeshank@purdue.edu>
2022-02-14 16:05:28 -0500

    Finished the account recovery process on the frontend side.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,8 @@ import Login from './routes/Login';
 import Profile from './Profile';
 import Signup from './routes/Signup';
 import {BrowserRouter, hashHistory, Route, Link, Routes} from "react-router-dom";
-import Forgotpassword from "./routes/ForgotPassword";
+import ForgotPassword from "./routes/ForgotPassword";
+import RecoverAccount from "./routes/RecoverAccount";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,7 +16,8 @@ ReactDOM.render(
           <Routes>
               <Route path="/Login" element={<Login />} />
               <Route path="/Signup" element={<Signup />} />
-              <Route path="/ForgotPassword" element={<Forgotpassword />} />
+              <Route path="/ForgotPassword" element={<ForgotPassword />} />
+              <Route path="/RecoverAccount" element={<RecoverAccount />} />
           </Routes>
       </BrowserRouter>
   </React.StrictMode>,
---
+++ frontend/src/routes/RecoverAccount.js
@@ -0,0 +1,43 @@
+import './RecoverAccount.css';
+
+function RecoverAccount() {
+    return (
+        <body>
+        <header className="Recover-account-header">
+            <p className="Recover-account-text">
+                Enter your new password and the code you received through email.
+            </p>
+            <label for="Recover-account-password">
+                <input className="Recover-account-password-field"
+                       type="text"
+                       id="recover-account-password"
+                       name="recover-account-password"
+                       placeholder="New Password" />
+                <div className="space"></div>
+                <input className="Recover-account-confirm-password-field"
+                       type="text"
+                       id="recover-account-confirm-password"
+                       name="recover-account-confirm-password"
+                       placeholder="Confirm Password" />
+                <div className="space"></div>
+                <input className="Recover-account-code-field"
+                       type="text"
+                       id="recover-account-code"
+                       name="recover-account-code"
+                       placeholder="Recovery Code" />
+                <div className="space"></div>
+                <button className="Change-password-button"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            window.location.href='/Login';
+                        }}><b>Change Password</b>
+                </button>
+            </label>
+        </header>
+        </body>
+    );
+}
+
+export default RecoverAccount;
+
+
