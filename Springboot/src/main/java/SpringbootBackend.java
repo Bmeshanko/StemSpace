91cd0adc64b12cd63459aadca170d49ee8cc5884
Bmeshanko <bmeshank@purdue.edu>
2022-02-13 23:30:02 -0500

    Made Springboot Project take 2

---
+++ Springboot/src/main/java/SpringbootBackend.java
@@ -0,0 +1,3 @@
+package Springboot;
+public class SpringbootBackend {
+}
--- Springboot/src/test/java/net/stemspace/springboot/SpringbootApplicationTests.java
+++ Springboot/src/test/java/net/stemspace/springboot/SpringbootApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package net.stemspace.springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringbootApplicationTests {
 
 	@Test
 	void contextLoads() {
