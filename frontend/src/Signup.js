13a1aa8f14301f0062da8f0484eb1b706978cbe1
Bmeshanko <bmeshank@purdue.edu>
2022-02-14 13:50:01 -0500

    Adding text fields and buttons for Signup.

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -7,22 +7,29 @@ function Signup() {
                 <input className="Signup-username-field" type="text" id="username" name="username"
                        placeholder="Username"/>
             </label>
-            <label for="password">
-                <input className="Signup-password-field" type="text" id="password" name="password"
-                       placeholder="Password"/>
-            </label>
-            <label for="confirm-password">
-                <input className="Signup-confirm-password-field" type="text" id="confirm-password" name="confirm-password"
-                       placeholder="Confirm Password"/>
-            </label>
+            <div className="space"></div>
             <label for="email">
                 <input className="Signup-email-field" type="text" id="email" name="email"
                        placeholder="Email Address"/>
             </label>
+            <div className="space"></div>
             <label for="confirm-email">
                 <input className="Signup-confirm-email-field" type="text" id="confirm-email" name="confirm-email"
                        placeholder="Confirm Email"/>
             </label>
+            <div className="space"></div>
+            <label htmlFor="password">
+                <input className="Signup-password-field" type="text" id="password" name="password"
+                       placeholder="Password"/>
+            </label>
+            <div className="space"></div>
+            <label htmlFor="confirm-password">
+                <input className="Signup-confirm-password-field" type="text" id="confirm-password"
+                       name="confirm-password"
+                       placeholder="Confirm Password"/>
+            </label>
+            <div className="space"></div>
+            <button className="Signup-button2" onClick="myFunction()"><b>Sign Up</b></button>
         </body>
     );
 }
