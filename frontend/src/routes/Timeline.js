3551afdf4e859d817edff8fb1d3ebd44de29122e
Brandon Huang <huan1533@purdue.edu>
2022-02-16 13:40:57 -0500

    Touched-up header, added scrollable sections for posts and chats/dms

--- frontend/src/routes/Timeline.js
+++ frontend/src/routes/Timeline.js
@@ -4,7 +4,10 @@ import {Link} from "react-router-dom";
 function Timeline() {
     return(
         <body>
-            <p className="Timeline-banner">StemSpace</p>
+            <p className="Timeline-banner">
+                <img className='Logo-position' src="Logo_new.png" alt="STEM"></img>
+                <p className='Stem-text'>StemSpace</p>
+            </p>
             <div className="Timeline-bar-horizontal"/>
             <header className="Timeline-selector">
                 <p className="Timeline-following">Following</p>
@@ -12,6 +15,12 @@ function Timeline() {
                 <p className="Timeline-topics">Topics</p>
             </header>
             <div className="Timeline-bar-horizontal"/>
+            <span class="posts">
+                posts
+            </span>
+            <span class="chats">
+                <p className="DM-header">Chats</p>
+            </span>
         </body>
     );
 }
