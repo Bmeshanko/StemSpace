52757040ea295f112b15151dcb2b397d0d21cee4
Bmeshanko <bmeshank@purdue.edu>
2022-02-17 15:22:56 -0500

    Updating timeline elements.

--- frontend/src/routes/Timeline.js
+++ frontend/src/routes/Timeline.js
@@ -4,30 +4,30 @@ import {Link} from "react-router-dom";
 function Timeline() {
     return(
         <body className="wrapper">
-        <p className="Timeline-banner">StemSpace
-            <img src="DM.png"
-                 className="DM-Logo"
-                 alt="DM"/>
-            <img src="Notification.png"
-                 className="Notification-Logo"
-                 alt="Notification"/>
-        </p>
-        <div className="Timeline-bar-horizontal"/>
-        <header className="Timeline-selector">
-            <p className="Timeline-following">Following</p>
-            <div className="Timeline-bar-vertical"/>
-            <p className="Timeline-topics">Topics</p>
-        </header>
-        <div className="Timeline-bar-horizontal"/>
-        <header className="Direct-messages">
+            <p className="Timeline-banner">StemSpace
+                <img src="DM.png"
+                     className="DM-Logo"
+                     alt="DM"/>
+                <img src="Notification.png"
+                     className="Notification-Logo"
+                     alt="Notification"/>
+            </p>
+            <div className="Timeline-bar-horizontal"/>
+            <header className="Timeline-selector">
+                <p className="Timeline-following">Following</p>
+                <div className="Timeline-bar-vertical"/>
+                <p className="Timeline-topics">Topics</p>
+            </header>
+            <div className="Timeline-bar-horizontal"/>
+            <header className="Direct-messages">
 
-        </header>
-        <span class="Timeline-posts">
-                posts
-        </span>
-        <span class="Timeline-DMs">
-                <p className="DM-header">Chats</p>
-        </span>
+            </header>
+            <span class="Timeline-posts">
+                    posts
+            </span>
+            <span class="Timeline-DMs">
+                    <p className="DM-header">Chats</p>
+            </span>
         </body>
     );
 }
