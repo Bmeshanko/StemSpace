59a0732ccc3eece50ab8b16f9248760d2149dbf2
JumpyCode <chi55@purdue.edu>
2022-02-19 14:03:47 -0500

    Add the function to edit the profile picture based

--- frontend/src/routes/Profile.js
+++ frontend/src/routes/Profile.js
@@ -2,24 +2,35 @@ import './Profile.css';
 import { Component } from 'react';
 class Profile extends Component{
   state = {
-    bio: 'I hate myself',
-    showName: false
+    bio: 'Nothing Here',
+    showName: false ,
+    showProfile: false ,
+    image: "Blank-Profile.png"
   }
   displayNameHandler = (e) => {
     let updatedBio = e.target.value;
     this.setState({ bio: updatedBio });
   }
-  handleSubmit = (e) => {
+  handleBioSubmit = (e) => {
     e.preventDefault();
     this.setState({
-      showName: true
+      showName: true,
     });
   }
-  handleClose = (e) => {
+  handleBioClose = (e) => {
     e.preventDefault();
     this.setState({
       showName: false
     });
+  }
+  onImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      let reader = new FileReader();
+      reader.onload = (e) => {
+        this.setState({image: e.target.result});
+      };
+      reader.readAsDataURL(event.target.files[0]);
+    }
   }
    render(){
       return (<body>
@@ -28,26 +39,27 @@ class Profile extends Component{
              <p className='Stem-text'>StemSpace</p>
          </p>
          <header className="Profile-bio">
-             <img className='Profile-picture' src="Blank-Profile.png" alt="STEM" ></img>
+             <img className='Profile-picture' src={this.state.image}></img>
              <span className="Profile-info">
                <p class="username">@UserName</p>
                {/* MAX CHAR for bio about 1500 */}
                <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>Bio:</label>
-          <button type="submit" onClick={this.handleSubmit}>Edit</button>
-          {this.state.showName && <input type="text" name="firstName" onChange={this.displayNameHandler} value={this.state.firstName} />}
+                  <form onSubmit={this.handleSubmit}>
+                    <label>Bio:</label>
+                    <button type="submit" onClick={this.handleBioSubmit}>Edit Bio</button>
+                    
+                    {this.state.showName && <input type="text" name="firstName" onChange={this.displayNameHandler} value={this.state.firstName} />}
 
-          {this.state.showName && <button type="button" onClick={this.handleClose}>Close</button>}
-          {<p>{this.state.bio}</p>}
-        </form>
-      </div>
+                    {this.state.showName && <button type="button" onClick={this.handleBioClose}>Close</button>}
+                    {<p>{this.state.bio}</p>}
+
+                    <input type="file" onChange={this.onImageChange} className="filetype" id="group_image" />
+                  </form>
+              </div>
              </span>
          </header>
          <section class="Post-history">
-             Post History
-             <p></p>
-     
+             <p>Post history</p>
          </section>
      </body>);
    }
