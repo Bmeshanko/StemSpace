125246e193553a513d3ae84accba4373dbda2996
bmeshanko <bmeshank@purdue.edu>
2022-03-22 14:29:37 -0400

    Working on frontpage.

--- frontend/src/routes/Front.js
+++ frontend/src/routes/Front.js
@@ -5,7 +5,17 @@ function Front() {
 
     return(
         <body>
+            <section className="Front-left">
+                <p className="Front-left-text">
+                    StemSpace is bringing the STEM community together <br/>
+                    by fostering a sense of inclusion within fields.
+                </p>
+                <img className='Front-logo' src="Big_logo.png" alt="STEM"></img>
+            </section>
 
+            <section className="Front-right">
+
+            </section>
         </body>
     );
 }
