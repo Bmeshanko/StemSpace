c40a55f1b1d4d33227fdde636234949ea86cb9e8
bmeshanko <bmeshank@purdue.edu>
2022-03-30 15:08:00 -0400

    Standard Web Flow working - getting more buttons together.

--- frontend/src/routes/Profile.js
+++ frontend/src/routes/Profile.js
@@ -14,104 +14,128 @@ const UseNavigate= ()=>{
 	return null;
 }
 class Profile extends Component {
-  	constructor() {
-    	super();
-    	this.state = {
+	constructor() {
+		super();
+		this.state = {
 			username:'',
-      		bio: '',
-      		showName: false ,
-      		showProfile: false ,
-      		image: ""
+			bio: '',
+			showName: false ,
+			showProfile: false ,
+			image: ""
 		}
 	}
 	componentDidMount(){
-    	this.setState({username:location.state.username})
-    	axios.post("/getUsers", {
-        	username: location.state.username
+		this.setState({username:location.state.username})
+		axios.post("/getUsers", {
+			username: location.state.username
 		}).then(res => {
-        	//console.log(res);
-        	if(res.data == null) {
-            	alert("Profile not Found")
-        	} else {
-            	this.setState({bio: res.data.bio});
-            	var base64Flag = 'data:image/jpeg;base64,';
-            	var imageStr = this.arrayBufferToBase64(res.data.img.data.data);
-            	this.setState({image: base64Flag + imageStr});
-            	//console.log(this.state.image);
-        	}
-    	}).catch(function (error) {
-        	console.log("Error Detected")
-    	})
-  	}
+			console.log(res);
+			if(res.data == null) {
+				alert("Profile not Found")
+			} else {
+				this.setState({bio: res.data.bio});
+				var base64Flag = 'data:image/jpeg;base64,';
+				var imageStr = this.arrayBufferToBase64(res.data.img.data.data);
+				this.setState({image: base64Flag + imageStr});
+				console.log(this.state.image);
+			}
+		}).catch(function (error) {
+			console.log("Error Detected")
+		})
+	}
 	displayNameHandler = (e) => {
-    	let updatedBio = e.target.value;
-    	this.setState({ bio: updatedBio });
-  	}
-  	onImageChange = (event) => {
-    	if (event.target.files && event.target.files[0]) {
-      	let reader = new FileReader();
-      	reader.onload = (e) => {
-        	this.setState({image: e.target.result});
-        	axios.post("/editImage", {
-          		image: this.state.image,
-          		username: this.state.username
-        	}).then(res => {
-            console.log(res.data);
-        	})
-      	};
-      	reader.readAsDataURL(event.target.files[0]);
-      	//console.log(event.target.files[0])
-    	}
-  	}
-  	arrayBufferToBase64(buffer) {
-    	var binary = '';
-    	var bytes = [].slice.call(new Uint8Array(buffer));
-    	bytes.forEach((b) => binary += String.fromCharCode(b));
-    	return window.btoa(binary);
-  	};
-	handleClickPost(event,user){
-		console.log(user)
-		navigate("/CreatePost", {state:{username:user}});
-	};
-	handleClickNotification(event,user) {
-		console.log(user)
-        navigate("/Profile", {state:{username:user}});
-    }
-	handleEditProfile(event,user) {
-		console.log(user)
-        navigate("/EditProfile", {state:{username:user}});
-    }
-   	render(){
-      	return (  
-      	<body>
-        	<UseLocation />
-			<UseNavigate />
-			<div className="Timeline-banner">
-				<button className="Timeline-logo-button"
-						onClick={(e) => {
-							this.handleClickLogo(e, this.state.username)
-						}}
-				><b><img className='Timeline-logo' src="Logo_new.png" alt="STEM"></img></b>
-				</button>
+		let updatedBio = e.target.value;
+		this.setState({ bio: updatedBio });
+	}
+	handleBioSubmit = (e) => {
+		e.preventDefault();
+		this.setState({
+			showName: true
+		});
+	}
+	handleBioClose = (e) => {
+		e.preventDefault();
+		axios.post("/editBio", {
+			bio: this.state.bio,
+			username: this.state.username
+		}).then(res => {
+			console.log(res.data);
+		});
 
-				<a className="Timeline-banner-text">StemSpace</a>
-				<button className="Notification-button"
-						onClick={(e) => {
-							this.handleClickPost(e, this.state.username)
-						}}
-				><b><img src="post_button.png" className="Notification-logo" alt="Create-post"/></b>
-				</button>
-				<button className="Notification-button"
-						onClick={(e) => {
-							this.handleClickNotification(e, this.state.username)
-						}}
-				><b><img src="Notification.png" className="Notification-logo" alt="Notification"/></b>
-				</button>
-			</div>
-			<div className="Timeline-bar-horizontal"/>
-         	<header className="Profile-bio">
-             	<img className='Profile-picture' src={this.state.image}></img>
-             	<span className="Profile-info">
+		this.setState({
+			showName: false
+		});
+	}
+	onImageChange = (event) => {
+		if (event.target.files && event.target.files[0]) {
+			let reader = new FileReader();
+			reader.onload = (e) => {
+				this.setState({image: e.target.result});
+				axios.post("/editImage", {
+					image: this.state.image,
+					username: this.state.username
+				}).then(res => {
+					console.log(res.data);
+				})
+			};
+			reader.readAsDataURL(event.target.files[0]);
+			//console.log(event.target.files[0])
+		}
+	}
+	arrayBufferToBase64(buffer) {
+		var binary = '';
+		var bytes = [].slice.call(new Uint8Array(buffer));
+		bytes.forEach((b) => binary += String.fromCharCode(b));
+		return window.btoa(binary);
+	};
+
+	handleClickPost(e, username) {
+		navigate("/CreatePost", {state:{username:username}});
+	}
+
+	handleClickNotification(e, username) {
+		navigate("/Profile", {state:{username:username}});
+	}
+
+	handleClickLogo(e, username) {
+		navigate("/Timeline", {state:{username:username}});
+	}
+
+	handleClickEdit(e, username) {
+		navigate("/EditProfile", {state:{username:username}});
+	}
+
+	render(){
+		return (
+			<body>
+				<UseLocation />
+				<UseNavigate />
+				<div className="Timeline-banner">
+					<button className="Timeline-logo-button"
+							onClick={(e) => {
+								this.handleClickLogo(e, this.state.username)
+							}}
+					><b><img className='Timeline-logo' src="Logo_new.png" alt="STEM"></img></b>
+					</button>
+
+					<a className="Timeline-banner-text">StemSpace</a>
+					<button className="Notification-button"
+							onClick={(e) => {
+								this.handleClickPost(e, this.state.username)
+							}}
+					><b><img src="post_button.png" className="Notification-logo" alt="Create-post"/></b>
+					</button>
+					<button className="Notification-button"
+							onClick={(e) => {
+								this.handleClickEdit(e, this.state.username)
+							}}
+					><b><img src="Notification.png" className="Notification-logo" alt="Notification"/></b>
+					</button>
+				</div>
+				<div className="Timeline-bar-horizontal"/>
+				<header className="Profile-bio">
+					<img className='Profile-picture' src={this.state.image}></img>
+					<span className="Profile-info">
                		<div>
                   		<form onSubmit={this.handleSubmit}>
 						  <p class="username">@{this.state.username}</p>
@@ -120,15 +144,16 @@ class Profile extends Component {
                   		</form>
               		</div>
              	</span>
-         	</header>
-			 <div class="Profile-Picture-Button"> 
-                	<label for="image">Update Profile Picture</label> 
-                	<input type="file" onChange={this.onImageChange} id="image" name="image" value="" required/> 
-            </div> 
-         	<section class="Post-history">
-            	<p>Post history</p>
-         	</section>
-     	</body>);
+				</header>
+				<div class="Profile-Picture-Button">
+					<label for="image">Update Profile Picture</label>
+					<input type="file" onChange={this.onImageChange} id="image" name="image" value="" required/>
+				</div>
+				<section class="Post-history">
+					<p>Post history</p>
+				</section>
+			</body>
+		);
 	}
 }
 export default Profile;
