e43e447ec244c97ad2ef4e40732ccb19affb1c52
zach-martin <70166918+zach-martin@users.noreply.github.com>
2022-04-11 00:26:07 -0400

    followers page/user story 10 basically done

--- frontend/src/routes/Followers.js
+++ frontend/src/routes/Followers.js
@@ -2,11 +2,11 @@ import {Link, useLocation, useNavigate, useParams} from "react-router-dom";
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import './Timeline.css';
+import data from "bootstrap/js/src/dom/data";
 
 function Followers(){
     const navigate = useNavigate();
     const location = useLocation();
-    console.log(location.state.username)
     const {userid} = useParams();
     const [input, setInput] = useState({
             username: userid,
@@ -14,16 +14,36 @@ function Followers(){
             users: []
         }
     );
+    function arrayBufferToBase64(buffer) {
+        let binary = '';
+        let bytes = [].slice.call(new Uint8Array(buffer));
+        bytes.forEach((b) => binary += String.fromCharCode(b));
+        return window.btoa(binary);
+    };
+
     useEffect(()=>{
-        axios.post("/followPage", {username: userid, view: location.state.view}).then(res=>{
-            let temp = [];
+        axios.post("/followPage", {username: userid, view: location.state.view}).then(res=> {
+            let temp
             if (location.state.view === "following") {
-                setInput(({username: userid, size: res.data.following.length, users: res.data.following}))
-            }else {
-                setInput(({username: userid, size: res.data.followers.length, users: res.data.followers}))
+                temp = res.data.following
+            } else {
+                temp = res.data.followers
             }
+            let temp2 = [];
+            let promises=[];
+            for(let i = 0; i < temp.length; i++){
+                promises.push(axios.post("/getUsers", {
+                    username: temp[i]
+                }).then (resp=> {
+                    let base64Flag = 'data:image/jpeg;base64,';
+                    let imageStr = arrayBufferToBase64(resp.data.img.data.data);
+                    let picture=base64Flag+imageStr;
+                    temp2[i] = {user:{username: temp[i], image: picture}};
+                }))
+            }
+            Promise.all(promises).then(()=>setInput({username: location.state.username, size: temp.length, users: temp2}));
         })
-    },[])
+    },[location.state.view,userid,location.state.username])
 
     function handleClickName(event, name) {
         navigate(`/Profile/${name}`, {state:{username:location.state.username}});
@@ -31,18 +51,17 @@ function Followers(){
 
     return(
         <body>
-        <span>
                 <ol>
                     {input.users.map((user) => (
-                        <div>
-                            <button onClick={(e) => {
-                                handleClickName(e,user)}}>
-                                @{user}
+                        <div className={"Post"}>
+                            <img className='SmallerPost-picture' src={user.user.image}></img>
+                            <button class = "Post" onClick={(e) => {
+                                handleClickName(e,user.user.username)}}>
+                                @{user.user.username}
                             </button>
                         </div>
                     ))}
                 </ol>
-        </span>
         </body>
     );
 }
